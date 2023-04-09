abf63a01a8fa960c8c3bfa75823c45118be1250e
jonghun <jonghuncu@gmail.com>
2023-04-09 20:02:59 +0900

    feat : Security, JWT 관련 업데이트

--- src/main/java/com/present/GifticBe/configuration/EncoderConfig.java
+++ src/main/java/com/present/GifticBe/configuration/EncoderConfig.java
@@ -2,9 +2,11 @@ package com.present.GifticBe.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class EncoderConfig {
 
     @Bean
--- src/main/java/com/present/GifticBe/configuration/SecurityConfig.java
+++ src/main/java/com/present/GifticBe/configuration/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.present.GifticBe.configuration;
 
+import com.present.GifticBe.service.UserSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
@@ -13,44 +17,46 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity
-//                .httpBasic().disable()
-//                .csrf().disable()
-//                .cors().and()
-//                .authorizeRequests()
-//                .antMatchers("/api/**").permitAll()
-//                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // jwt사용하는 경우 씀
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
-//                .and()
-//                .build();
-//    }
+
+    private final UserSecurityService userSecurityService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-                httpBasic().disable()
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-                .and()
-                .headers()
-                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .antMatchers("/**").permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // jwt사용하는 경우
+                .antMatchers("/api/v1/users/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/api/v1/users/login")
+//                .usernameParameter("email")
+//                .passwordParameter("password")
                 .and()
+//                .authorizeRequests()
+//                .antMatchers("/api/v1/users/join").permitAll() // jwt사용하는 경우 씀
+//                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
-        ;
-
-        return http.build();
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/users/logout"))
+                .logoutUrl("/")
+                .invalidateHttpSession(true)
+                .and()
+                .getOrBuild()
+                ;
     }
 
     @Bean
